f89b8a71d19b60b2779ac77a633a6e2f8100755b
Daniel <danielwzk0107@gmail.com>
2022-10-15 23:34:19 +0800

    fix content

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,12 +7,11 @@ function HomePage() {
       <figure className="theme-bgc">
         <section className="theme-bgc__border">
           <main className="home container">
-            <section className="home__circle"></section>
             <Link to="/Project">
               <section className="home__title">
                 <h1>FRONT-END</h1>
                 <h1>ENGINEER</h1>
-                <h1>+ART</h1>
+                <h1>+UI/UX</h1>
                 <h1>DESIGNER</h1>
               </section>
             </Link>
